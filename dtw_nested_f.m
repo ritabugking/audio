69ed9a473bc7ea2b1d8631feebc1f32c169f24a3
ritabugking <ritabugking@gmail.com>
2017-08-22 00:50:57 -0700

    much more faster after refined

---
+++ dtw_nested_f.m
@@ -0,0 +1,139 @@
+[simi, locx, locy, path]=sim();
+path=reshape(path,2,[])';
+plot(path(:,2),path(:,1))
+
+function [similarity, location_x, location_y, path_] = sim () 
+
+
+filename = sprintf('/Users/yijuwang/Desktop/spto/yw/segment/seg4_3.wav');
+[x, Fs]=audioread(filename);
+%[x, Fs]=audioread(filename, [1,44100*1+1]);
+x=x(:,1); % one channel
+x=downsample(x,2);
+% bandpass filter 2000-10000Hz
+passBand=[1800, 11000];
+[bb, aa]=butter(8, passBand/(Fs/2));
+[h, w]=freqz(bb, aa);
+x=filter(bb, aa, x);
+
+% enframe: window length = 256, step = 128
+wlen=256; inc=128; win=hanning(wlen);
+N=length(x); time=(0:N-1)/Fs;      
+y=enframe(x,win,inc)';              
+fn=size(y,2);                       
+frameTime=(((1:fn)-1)*inc+wlen/2)/Fs; 
+W2=wlen/2+1; n2=1:W2;
+freq=(n2-1)*Fs/wlen;                
+Y=fft(y);                           
+ywdata=Y;
+%t=abs(ywdata); % potential spectrogram
+t=abs(ywdata); % potential spectrogram
+
+filename = sprintf('/Users/yijuwang/Desktop/spto/yw/ywt.wav');
+[x, Fs]=audioread(filename);
+x=x(:,1); % one channel
+x=downsample(x,2);
+% bandpass filter 2000-10000Hz
+passBand=[1800, 11000];
+[bb, aa]=butter(8, passBand/(Fs/2));
+[h, w]=freqz(bb, aa);
+x=filter(bb, aa, x);
+
+% enframe: window length = 256, step = 128
+wlen=256; inc=128; win=hanning(wlen);
+N=length(x); time=(0:N-1)/Fs;      
+y=enframe(x,win,inc)';              
+fn=size(y,2);                       
+frameTime=(((1:fn)-1)*inc+wlen/2)/Fs; 
+W2=wlen/2+1; n2=1:W2;
+freq=(n2-1)*Fs/wlen;                
+Y=fft(y);                           
+ywt=Y;
+s=abs(ywt); % template spectrogram
+
+% dynamic time warping
+w=-Inf;
+ns=size(s,2);
+nt=size(t,2);
+if size(s,1)~=size(t,1)
+    error('Error in dtw(): the dimensions of the two input signals do not match.');
+end
+
+%% initialization
+D=zeros(ns+2,nt+2)-Inf; % cache matrix
+D(1,1)=0;
+D(2,2)=0;
+
+% similarity matrix (cosing similarity)
+oost = zeros(ns+1,nt+1)-Inf;
+for i=1:ns
+    for j=1:nt
+        oost(i+1,j+1) = (dot(s(:,i),t(:,j))/(norm(s(:,i),2)*norm(t(:,j),2))); % = cos(theta)
+    end
+end
+       
+
+%% begin dynamic programming
+%find the maximal similarity between two matrix s(w*ns) & t(w*nt)
+% the start point should be aligned, but the end point doesn't need to be
+for i=1:ns
+    for j=1:nt
+        D(i+2,j+2)=oost(i+1,j+1)+max([D(i,j+1)+oost(i,j+1), D(i+1,j)+oost(i+1,j), D(i+1,j+1)]);
+    end
+end
+
+n_max=max(D(:,1:size(D,2))); % ????????????????
+big=-Inf;
+loc=-Inf;
+for i=3:size(D,2)
+    if (big<n_max(i)/(i+size(D,1)))
+        big=n_max(i)/(i+size(D,1));
+        loc=i;
+    end
+end
+
+similarity = big;
+location_x = size(D,1);
+location_y = loc;
+
+
+
+%=========================================================================
+[path, x1, x2]=find_path(size(D,1)-2,size(D,2)-2);
+
+function [pathmap, cord1, cord2] = find_path(a,b)
+persistent oo;
+if (a==0 || b==0)
+    cord1=0;
+    cord2=0;
+    oo =[oo, cord1, cord2];
+    pathmap=oo;
+    return;
+end
+
+if (max([D(a,b+1)+oost(a,b+1), D(a+1,b)+oost(a+1,b), D(a+1,b+1)])==D(a,b+1)+oost(a,b+1))
+    cord1=a-1;
+    cord2=b;
+    find_path(a-2,b-1);
+    oo =[oo, cord1, cord2];
+    oo =[oo, a, b];    
+    pathmap=oo;
+elseif (max([D(a,b+1)+oost(a,b+1), D(a+1,b)+oost(a+1,b), D(a+1,b+1)])==D(a+1,b)+oost(a+1,b))
+    cord1=a;
+    cord2=b-1;
+    find_path(a-1,b-2);
+    oo =[oo, cord1, cord2];
+    oo =[oo, a, b];   
+    pathmap=oo;
+elseif (max([D(a,b+1)+oost(a,b+1), D(a+1,b)+oost(a+1,b), D(a+1,b+1)])==D(a+1,b+1))
+    cord1=a;
+    cord2=b;
+    find_path(a-1,b-1);
+    oo =[oo, a, b];
+    pathmap=oo;
+end
+    
+end
+path_ = path;
+
+end
